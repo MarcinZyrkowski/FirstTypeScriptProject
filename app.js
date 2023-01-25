a56b8a4139032a57b23a93d8e46eb9b6bb8acf8f
MarcinZyrkowski <marcinzyrkowski7@gmail.com>
2023-01-25 18:22:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(number_1, number_2) {
+    return number_1 + number_2;
+}
+var x = 5;
+var y = 4.3;
+console.log(add(x, y));
---
+++ app.ts
@@ -0,0 +1,8 @@
+function add(number_1: number, number_2: number){
+    return number_1 + number_2;
+}
+
+const x = 5;
+const y = 4.3;
+
+console.log(add(x,y));
